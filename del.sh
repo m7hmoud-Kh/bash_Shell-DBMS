a6fecf41456ff3d4aa91cb1a8ecb94934d84c134
kasem <kasemsaad756@gmail.com>
2024-01-24 22:52:08 +0200

    deleteAndDeletAll

--- del.sh
+++ del.sh
@@ -2,6 +2,7 @@
 read -p "Enter name table" table
 read -p "Enter number of field" field
 read -p "enter value match to delete " value
+#read -p "enter value match to delete " value
 #awk -v vfield="$feild" -v vvalue="$value" '{ if($vfield == vvalue) print " " }' "$table"
                                                                                                                                                            شرط 
 awk -v vfield="$field" -v vvalue="$value" '$vfield != vvalue' "$table" > temp_file && mv temp_file "$table"
