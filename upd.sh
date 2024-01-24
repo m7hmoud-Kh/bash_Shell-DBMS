a6cf3f0c8d64f37f2e5d1ae3a2076f11417938e7
kasem <kasemsaad756@gmail.com>
2024-01-25 01:02:28 +0200

    update

---
+++ upd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+read -p "Enter name table" table
+read -p "Enter number of field" field
+read -p "enter old value" ovalue
+read -p "enter new value " nvalue
+read -p "Enter number of field condtion" cfield
+read -p "Enter value of field condtion" vcvalue
+#read -p "enter value match to delete " value
+#awk -v vfield="$feild" -v vvalue="$value" '{ if($vfield == vvalue) print " " }' "$table"
+                                                                                                                                                           شرط 
+awk -v vfield="$field" -v vvalue="$value" '$vfield != vvalue' "$table" > temp_file && mv temp_file "$table"
+echo delete row success
+
